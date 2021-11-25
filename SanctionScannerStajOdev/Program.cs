e64a92c022fedbba12650d28b0a41969b3d78953
melikber <melikbergngr@gmail.com>
2021-11-25 23:45:44 +0300

    first commit

---
+++ SanctionScannerStajOdev/Program.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace SanctionScannerStajOdev
+{
+    class Program
+    {
+        private static string htmlFileName = "sahibindenTest.html";
+
+        static void Main(string[] args)
+        {
+            var path = System.IO.Path.GetFullPath(@"..\..\..\");
+
+            GetDataAndShowVitrinTitles(path + htmlFileName);
+        }
+
+        private static void GetDataAndShowVitrinTitles(string path)
+        {
+            string htmlPageSource = File.ReadAllText(path);
+
+            var decodedHtmlPage = WebUtility.HtmlDecode(htmlPageSource);
+            HtmlDocument htmlDocument = new HtmlDocument();
+
+            htmlDocument.LoadHtml(decodedHtmlPage);
+
+            var firstVitrinList = htmlDocument.DocumentNode.Descendants().Where(x => (x.Name == "ul" && x.Attributes["class"] != null &&
+                                                                    x.Attributes["class"].Value.Equals("vitrin-list clearfix"))).ToList();
+            
+            var listHtmlString = firstVitrinList[0].InnerHtml;
+
+            var innerHtml = new HtmlDocument();
+            innerHtml.LoadHtml(listHtmlString);
+
+            var vitrinList = innerHtml.DocumentNode.Descendants().Where(c=>c.Name == "li").ToList();
+
+            foreach (var vitrinItem in vitrinList)
+            {
+                HtmlDocument vitrinHtml = new HtmlDocument();
+                vitrinHtml.LoadHtml(vitrinItem.InnerHtml);
+
+                var title = vitrinHtml.DocumentNode.InnerText;
+                Console.WriteLine(title.Trim());
+            }
+        }
+    }
+}
